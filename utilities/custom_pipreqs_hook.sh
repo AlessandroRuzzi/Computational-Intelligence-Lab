6081756b569253ea8f58de6f5f332a4c8bddd9a3
alessandroruzzi <alrxruzzi98@gmail.com>
2021-03-29 18:54:18 +0200

    updated requirements.txt still other things to change

--- utilities/custom_pipreqs_hook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pipreqs --ignore pipreqs_ignored,.mypy_cache --savepath utilities/pipreqs.txt
-sort utilities/non_imported_requirements.txt utilities/pipreqs.txt | uniq > requirements.txt
-rm utilities/pipreqs.txt

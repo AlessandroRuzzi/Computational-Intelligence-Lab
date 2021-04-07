dcada79b63da44f0ca51d76e31288795205547c6
Flavio Schneider <flavio.schneider.97@gmail.com>
2021-04-07 16:17:13 +0200

    Added GPU training configs.

--- leonhard_init.sh
+++ leonhard_init.sh
@@ -1,4 +1,6 @@
-module load gcc/6.3.0 python_gpu/3.8.5
+module load eth_proxy gcc/6.3.0 python_gpu/3.8.5
+python -m venv venv 
 source venv/bin/activate
+module load eth_proxy gcc/6.3.0 python_gpu/3.8.5
 pip3 install -r leonhard_requirements.txt  
 export PYTHONPATH=$PYTHONPATH:~/Computational-Intelligence-Lab-2021
---
+++ leonhard_run.sh
@@ -0,0 +1,6 @@
+# Train on 64 GB of RAM, 1 GPU (Tesla with 32GB memory)
+# bsub -R "rusage[mem=64000, ngpus_excl_p=1]" -R "select[gpu_model0==TeslaV100_SXM2_32GB]" python3 ./run.py trainer.gpus=1
+# Train on 64 GB of RAM, 1 CPU 
+# bsub -R "rusage[mem=64000]" python3 ./run.py 
+# Train on 64 GB of RAM, 8 GPUs (GeForceGTX1080 with 8GB of memory)
+bsub -R "rusage[mem=64000, ngpus_excl_p=8]" python3 ./run.py trainer.gpus=-1 

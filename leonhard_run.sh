04aa5670ddddd22e9a1838efb93a4f4b2caf9015
Flavio Schneider <flavio.schneider.97@gmail.com>
2021-04-11 16:33:44 +0200

    Fixed test experiment.

--- leonhard_run.sh
+++ leonhard_run.sh
@@ -5,6 +5,6 @@
 # Train on 64 GB of RAM, 8 GPUs (GeForceGTX1080 with 8GB of memory)
 #bsub -W 20:00 -R "rusage[mem=64000, ngpus_excl_p=1]" python3 ./run.py +experiment=exp_standard
 bsub -W 24:00 -R "rusage[mem=64000, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" python3 ./run.py +experiment=exp_standard
-
-bsub -W 01:00 -R "rusage[mem=64000, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" python3 ./run.py +experiment=exp_simple_unet_backboned +test=/cluster/home/scflavio/Computational-Intelligence-Lab-2021/logs/ckpts/2021-04-11/12-38-15/last.ckpt
+# Run test from checkpoint 
+bsub -W 01:00 -R "rusage[mem=64000, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" python3 ./run.py +experiment=test_simple_unet_backboned +model.checkpoint_path=/cluster/home/scflavio/Computational-Intelligence-Lab-2021/logs/ckpts/2021-04-11/12-38-15/last.ckpt
 

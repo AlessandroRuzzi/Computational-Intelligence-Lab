1e036c22c1a723b0dec08f775a1bfd88c9d7a5aa
Alessandro Ruzzi <alrxruzzi98@gmail.com>
2021-04-12 15:49:53 +0200

    Update leonhard_init.sh
    change in Leonard script

--- leonhard_init.sh
+++ leonhard_init.sh
@@ -2,5 +2,5 @@ module load eth_proxy gcc/6.3.0 python_gpu/3.8.5
 python -m venv venv 
 source venv/bin/activate
 module load eth_proxy gcc/6.3.0 python_gpu/3.8.5
-pip3 install -r leonhard_requirements.txt  
+pip3 install -r requirements.txt  
 export PYTHONPATH=$PYTHONPATH:~/Computational-Intelligence-Lab-2021

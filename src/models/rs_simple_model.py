6211f702b58f94fdba4904350851527ea300d547
MirkoDeVita98 <mirko.devita@icloud.com>
2021-07-21 13:46:58 +0200

    first try with test sizes

--- src/models/rs_simple_model.py
+++ src/models/rs_simple_model.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List, Sequence, Tuple, Union
-
+import numpy as np
 import pytorch_lightning as pl
 import torch
 from torch.optim import Optimizer
@@ -8,6 +8,43 @@ import src.utils.model_utils as utils
 from src.models.metrics.kaggle_accuracy import KaggleAccuracy
 
 # from src.models.metrics.dice_loss import BinaryDiceLoss
+
+def split_picture(test_picture):
+    split_1 = test_picture[:400, :400]
+    split_2 = test_picture[:400, -400:]
+    split_3 = test_picture[-400:, :400]
+    split_4 = test_picture[-400:, -400:]
+    return [split_1, split_2, split_3, split_4]
+
+
+def merge_splits(split_1, split_2, split_3, split_4, mode='mean'):
+    assert mode in ['mean', 'max'], 'mode can only be one between mean and max!'
+    if mode == 'mean':
+        function = np.mean
+    elif mode == 'max':
+        function = np.max
+    intersect1 = function(np.array([split_1[:208, 208:], split_2[:208, :192]]), axis=0)
+    intersect2 = function(np.array([split_3[192:, 208:], split_4[192:, :192]]), axis=0)
+
+    intersect3 = function(np.array([split_1[208:, :208], split_3[:192, :208]]), axis=0)
+    intersect4 = function(np.array([split_2[208:, 192:], split_4[:192, 192:]]), axis=0)
+
+    intersect5 = function(np.array([split_1[208:, 208:], 
+                                split_2[208:, :192],
+                                split_3[:192, 208:],
+                                split_4[:192, :192]]), axis=0)
+
+    north_west = split_1[:208, :208]
+    north_east = split_2[:208, 192:]
+    south_west = split_3[192:, :208]
+    south_east = split_4[192:, 192:]
+
+    upper_slice = np.concatenate([north_west, intersect1, north_east], axis= 1)
+    middle_slice = np.concatenate([intersect3, intersect5, intersect4], axis= 1)
+    lower_slice = np.concatenate([south_west, intersect2, south_east], axis= 1)
+
+    final_image = np.concatenate([upper_slice, middle_slice, lower_slice], axis= 0)
+    return final_image
 
 class RSSimpleModel(pl.LightningModule):
     def __init__(
@@ -75,9 +112,28 @@ class RSSimpleModel(pl.LightningModule):
 
     def test_step(self, batch: torch.Tensor, batch_id: int) -> Dict[str, torch.Tensor]:
         x, kaggle_ids = batch
-        preds_proba = torch.sigmoid(self.forward(x))
-        preds_discr = (preds_proba > 0.5).float()
 
+        print("X SHAPE: " , x.shape)
+
+        lst1 = []
+        lst2 = []
+        for i in range(0, x.shape[0]):
+            split_1, split_2, split_3, split_4 = split_picture(x[i,:, :])
+            preds_proba1 = torch.sigmoid(self.forward(split_1)).numpy()
+            preds_proba2 = torch.sigmoid(self.forward(split_2)).numpy()
+            preds_proba3 = torch.sigmoid(self.forward(split_3)).numpy()
+            preds_proba4 = torch.sigmoid(self.forward(split_4)).numpy()
+
+            preds_proba = torch.from_numpy(merge_splits(preds_proba1, preds_proba2, preds_proba3, preds_proba4, mode = 'max'))
+            preds_discr = (preds_proba > 0.5).float()
+
+            lst1.append(preds_proba)
+            lst2.append(preds_discr)
+        
+
+        preds_proba = torch.stack(lst1)
+        preds_discr = torch.stack(lst2)
+        
         # Log prediction images
         image_grid = utils.build_image_grid_test(x, preds_proba).cpu()
         self.logger[0].experiment.log_image(image_grid, "Test Batch")

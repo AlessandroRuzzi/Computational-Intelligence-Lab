12775192bfa5b6e6fb91d40c92b77bf542e608b9
MirkoDeVita98 <mirko.devita@icloud.com>
2021-07-23 00:25:02 +0200

    unfold strategy test sizes

--- src/models/rs_simple_model.py
+++ src/models/rs_simple_model.py
@@ -1,50 +1,15 @@
+from os import X_OK
 from typing import Any, Dict, List, Sequence, Tuple, Union
 import numpy as np
 import pytorch_lightning as pl
 import torch
 from torch.optim import Optimizer
+import torch.nn.functional as F
 
 import src.utils.model_utils as utils
 from src.models.metrics.kaggle_accuracy import KaggleAccuracy
 
 # from src.models.metrics.dice_loss import BinaryDiceLoss
-
-def split_picture(test_picture):
-    split_1 = test_picture[:400, :400]
-    split_2 = test_picture[:400, -400:]
-    split_3 = test_picture[-400:, :400]
-    split_4 = test_picture[-400:, -400:]
-    return [split_1, split_2, split_3, split_4]
-
-
-def merge_splits(split_1, split_2, split_3, split_4, mode='mean'):
-    assert mode in ['mean', 'max'], 'mode can only be one between mean and max!'
-    if mode == 'mean':
-        function = np.mean
-    elif mode == 'max':
-        function = np.max
-    intersect1 = function(np.array([split_1[:208, 208:], split_2[:208, :192]]), axis=0)
-    intersect2 = function(np.array([split_3[192:, 208:], split_4[192:, :192]]), axis=0)
-
-    intersect3 = function(np.array([split_1[208:, :208], split_3[:192, :208]]), axis=0)
-    intersect4 = function(np.array([split_2[208:, 192:], split_4[:192, 192:]]), axis=0)
-
-    intersect5 = function(np.array([split_1[208:, 208:], 
-                                split_2[208:, :192],
-                                split_3[:192, 208:],
-                                split_4[:192, :192]]), axis=0)
-
-    north_west = split_1[:208, :208]
-    north_east = split_2[:208, 192:]
-    south_west = split_3[192:, :208]
-    south_east = split_4[192:, 192:]
-
-    upper_slice = np.concatenate([north_west, intersect1, north_east], axis= 1)
-    middle_slice = np.concatenate([intersect3, intersect5, intersect4], axis= 1)
-    lower_slice = np.concatenate([south_west, intersect2, south_east], axis= 1)
-
-    final_image = np.concatenate([upper_slice, middle_slice, lower_slice], axis= 0)
-    return final_image
 
 class RSSimpleModel(pl.LightningModule):
     def __init__(
@@ -113,29 +78,38 @@ class RSSimpleModel(pl.LightningModule):
     def test_step(self, batch: torch.Tensor, batch_id: int) -> Dict[str, torch.Tensor]:
         x, kaggle_ids = batch
 
-        print("X SHAPE: " , x.shape)
+        batch_size = x.shape[0]
+        splits = 4
 
-        lst1 = []
-        lst2 = []
-        for i in range(0, x.shape[0]):
-            split_1, split_2, split_3, split_4 = split_picture(x[i,:, :])
-            preds_proba1 = torch.sigmoid(self.forward(split_1)).numpy()
-            preds_proba2 = torch.sigmoid(self.forward(split_2)).numpy()
-            preds_proba3 = torch.sigmoid(self.forward(split_3)).numpy()
-            preds_proba4 = torch.sigmoid(self.forward(split_4)).numpy()
+        dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
-            preds_proba = torch.from_numpy(merge_splits(preds_proba1, preds_proba2, preds_proba3, preds_proba4, mode = 'max'))
-            preds_discr = (preds_proba > 0.5).float()
+        kernel_size = 400
+        stride = kernel_size//2 
+        x_unf = F.unfold(x, kernel_size, stride=stride)
 
-            lst1.append(preds_proba)
-            lst2.append(preds_discr)
+        x_unf = x_unf.permute(0, 2, 1)
+        x_unf = x_unf.view(batch_size * splits, 3, 400, 400)
+
+        splits_pred = []
+        for split in (0, splits):
+            pred = torch.sigmoid(self.forward(x_unf[batch_size * split : batch_size * split + batch_size, : , :]))
+            splits_pred.append(pred)
         
+        preds_proba = torch.stack(splits_pred)
+        preds_proba = preds_proba.view(batch_size, 3 * 400 * 400, splits)
 
-        preds_proba = torch.stack(lst1)
-        preds_discr = torch.stack(lst2)
+        pred_f = F.fold(preds_proba,x.shape[-2:],kernel_size,stride=stride)
+        norm_map = F.fold(F.unfold(torch.ones(x.shape).type(dtype),kernel_size,stride=stride),x.shape[-2:],kernel_size,stride=stride)
+        pred_f /= norm_map
+
+        temp = F.fold(torch.ones(batch_size * splits, 3, 400, 400),x.shape[-2:],kernel_size,stride=stride)
+        temp /= norm_map
+
+        pred_f /= temp
+        preds_discr = (pred_f > 0.5).float()
         
         # Log prediction images
-        image_grid = utils.build_image_grid_test(x, preds_proba).cpu()
+        image_grid = utils.build_image_grid_test(x, pred_f).cpu()
         self.logger[0].experiment.log_image(image_grid, "Test Batch")
 
         # Save predictions images to folder
